77df124814b137ed7e9bc058ea8d2ba88ed3032f
Charvi Gopal <charvig@mit.edu>
2020-05-06 12:37:34 -0400

    Add pca-6

--- linear_regression_pca_6.py
+++ linear_regression_pca_6.py
@@ -4,7 +4,7 @@ import statsmodels.api as sm
 from statsmodels.tools.eval_measures import rmse
 
 # get training data 
-X_training = pd.read_csv("data/pc4_ev[ 0.3413183   0.25478867  0.13134266  0.10615022]_train_x.csv",header=None)
+X_training = pd.read_csv("data/pc6_ev[ 0.3413183   0.25478867  0.13134266  0.10615022  0.06533687  0.05613034]_train_x.csv",header=None)
 y_training = pd.read_csv("data/train_y.csv",header=None)
 
 # train
@@ -15,7 +15,7 @@ rmse_pred = rmse(y_training[0].values.tolist(), predictions.values.tolist())
 print("the RMSE is:", rmse_pred)'''
 
 # get validation data
-X_validation = pd.read_csv("data/pc4_ev[ 0.3413183   0.25478867  0.13134266  0.10615022]_val_x.csv",header=None)
+X_validation = pd.read_csv("data/pc6_ev[ 0.3413183   0.25478867  0.13134266  0.10615022  0.06533687  0.05613034]_val_x.csv",header=None)
 y_validation = pd.read_csv("data/val_y.csv",header=None)
 
 # validate
