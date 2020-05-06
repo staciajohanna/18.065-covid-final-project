0d459478acdff424de35b743549e67bc5ab3e856
Charvi Gopal <charvig@mit.edu>
2020-05-06 11:34:48 -0400

    Validate non-PCA data

--- linear_regression_no_pca.py
+++ linear_regression_no_pca.py
@@ -3,15 +3,21 @@ import pandas as pd
 import statsmodels.api as sm
 from statsmodels.tools.eval_measures import rmse
 
+# get training data 
 all_data = pd.read_csv("data/std_train_x.csv",header=None)
-df = all_data
-target = pd.read_csv("data/train_y.csv",header=None)
+X_training = all_data
+y_training = pd.read_csv("data/train_y.csv",header=None)
 
-#training 
-X_training = df
-y_training = target
+# train
 model = sm.OLS(y_training, X_training).fit()
 
-predictions = model.predict(X_training)
-rmse_pred = rmse(y_training[0].values.tolist(), predictions.values.tolist())
+# get validation data
+all_data = pd.read_csv("data/std_val_x.csv",header=None)
+X_validation = all_data
+y_validation = pd.read_csv("data/val_y.csv",header=None)
+
+# validate
+predictions = model.predict(X_validation)
+rmse_pred = rmse(y_validation[0].values.tolist(), predictions.values.tolist())
 print("the RMSE is:", rmse_pred)
+
